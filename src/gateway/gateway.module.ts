95c5f3ba1366a81f4025c706496b2d3f0ec0da9e
Daniil Doletskyi <daniil.doletskyi@bluepes.com>
2022-12-01 21:05:51 +0200

    feat: setup socket.io

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Message, MessageSchema } from 'src/messages/messages.schema';
+import { MessagesService } from 'src/messages/messages.service';
 import { SocketGateway } from './gateway';
 
 @Module({
-  providers: [SocketGateway],
+  imports: [
+    MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
+  ],
+  providers: [MessagesService, SocketGateway],
 })
 export class GatewayModule {}
--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,13 +1,21 @@
 import {
   WebSocketGateway,
   SubscribeMessage,
-  MessageBody,
+  WebSocketServer,
 } from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { MessagesService } from 'src/messages/messages.service';
 
 @WebSocketGateway()
 export class SocketGateway {
+  constructor(private readonly messageService: MessagesService) {}
+
+  @WebSocketServer()
+  server: Server;
+
   @SubscribeMessage('newMessage')
-  onNewMessage(@MessageBody() body: any) {
-    console.log(body);
+  async onNewMessage() {
+    const messages = await this.messageService.findAll();
+    this.server.sockets.emit('newMessage', messages);
   }
 }
